94bbc112a5e8c3c1b839d05ef362e985642a3714
ayinembabaziphionah <61048762+ayinembabaziphionah@users.noreply.github.com>
2020-11-26 07:19:01 +0300

    first commit

---
+++ app/src/main/java/com/example/gigsemployeremployeeapp/Queries.kt
@@ -0,0 +1,12 @@
+package com.example.gigsemployeremployeeapp
+
+//import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Queries : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_queries)
+    }
+}
